86a1499435dc4b687592aace879cd3a65a4abd31
Gabjea <m.beaudoinmarc@gmail.com>
2026-02-15 00:10:37 +0100

    add const &modif main

---
+++ prod/src/config.rs
@@ -0,0 +1,2 @@
+pub const TOKEN0_DECIMAL: i32 = 18;
+pub const TOKEN1_DECIMAL: i32 = 6; 
--- prod/src/main.rs
+++ prod/src/main.rs
@@ -1,6 +1,32 @@
 mod live_feed;
+mod market;
+mod config;
+use live_feed::LiveEvent;
+use market::state::MarketState;
+use tokio::sync::mpsc;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    live_feed::livefeed::start_livefeed().await
+
+    // Create Market State
+    let mut market_state: MarketState = Default::default();
+
+    // Initialize & Run LiveFeed
+    let (tx, mut rx) = mpsc::channel::<LiveEvent>(1024);
+    tokio::spawn(live_feed::livefeed::start_livefeed(tx));
+
+    // Central Loop : Triggered when Event received
+    while let Some(event) = rx.recv().await {
+        market_state.update(&event);
+        
+        match event {
+            LiveEvent::Uniswap { .. } => {
+                market_state.is_imbalanced();
+            }
+            LiveEvent::Hyperliquid { timestamp, levels } => {
+                // println!("[HYPER] {} , {:?}",timestamp,levels );
+            }
+        }
+    }
+    Ok(())
 }
